9af0479cd39eba94a0c79b31b7a1a3f62e9a07c3
Pradyumna Thorat <thoratpradyumna@gmail.com>
2023-03-24 18:07:19 +0530

    first

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false)
 const connect = async () =>  {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect("mongodb+srv://Pradyumna:Pradyumna@cluster0.qxvv1hh.mongodb.net/Task_manager");
     console.log("Connected to Database");
 }
 
